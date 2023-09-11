5a5e556fa861e59827747e740e1804feb6dbdc77
StandeMan15 <99266225+StandeMan15@users.noreply.github.com>
2023-09-11 10:42:08 +0200

    added bmi and age calculation

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 import java.time.LocalDate;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Main {
     public static void main(String[] args) {
         Patient[] patients = {
-                new Patient("John Doe", "1999-01-01", "1234AB", 81),
-                new Patient("Tim Timmer", "1976-12-31", "1111AB", 95)
+                new Patient("John Doe", "1999-01-01", "1234AB", 81, 178),
+                new Patient("Tim Timmer", "1976-12-31", "1111AB", 95, 192)
         };
 
         Scanner scanRole = new Scanner(System.in);
@@ -22,9 +24,12 @@ public class Main {
             if (foundPatient != null) {
                 System.out.println("Patient found in the array:");
                 System.out.println("Full Name: " + foundPatient.getFullName());
-                System.out.println("Birth Date: " + foundPatient.getBirthDate());
                 System.out.println("Postal Code: " + foundPatient.getPostalCode());
                 System.out.println("Weight: " + foundPatient.getWeight());
+                System.out.println("Lengte: " + foundPatient.getLength() + "cm");
+                System.out.println("BMI: " + foundPatient.getBMI());
+                System.out.println("Birth Date: " + foundPatient.getBirthDate());
+                System.out.println("Leeftijd: " + foundPatient.getAge() + " jaar");
             } else {
                 System.out.println("Patient not found in the array.");
             }
@@ -51,12 +56,14 @@ class Patient {
     private LocalDate birthDate;
     private String postalCode;
     private int weight;
+    private int length;
 
-    public Patient(String fullName, String birthDateStr, String postalCode, int weight) {
+    public Patient(String fullName, String birthDateStr, String postalCode, int weight, int length) {
         this.fullName = fullName;
         this.birthDate = LocalDate.parse(birthDateStr);
         this.postalCode = postalCode;
         this.weight = weight;
+        this.length = length;
     }
 
     public String getFullName() {
@@ -73,5 +80,21 @@ class Patient {
 
     public int getWeight() {
         return weight;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public double getBMI() {
+        double heightInMeters = (double) length / 100.0; 
+        double bmi = weight / (heightInMeters * heightInMeters);
+        return Math.round(bmi * 100.0) / 100.0;
+    }
+
+        public int getAge() {
+        LocalDate currentDate = LocalDate.now();
+        Period age = Period.between(birthDate, currentDate);
+        return age.getYears();
     }
 }
